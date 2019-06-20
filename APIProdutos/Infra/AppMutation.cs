71fcbd92223d216bd61e14655419033617568437
Thiagtao da Silva Adriano <tadriano@band.com.br>
2019-06-20 16:14:55 -0300

    subindo alteracoes mutation

--- APIProdutos/Infra/AppMutation.cs
+++ APIProdutos/Infra/AppMutation.cs
@@ -1,6 +1,7 @@
 ﻿using APIProdutos.Business;
 using APIProdutos.Models;
 using APIProdutos.Models.Schemas;
+using GraphQL;
 using GraphQL.Types;
 using System;
 using System.Collections.Generic;
@@ -28,8 +29,48 @@ namespace APIProdutos.Infra
                      var produto = context.GetArgument<Produto>("produto");
                      produtoService.Incluir(produto);
                      return produtoService.Obter(produto.CodigoBarras);
+                 });
+
+
+            Field<ProdutoType>(
+             "updateProduto",
+             arguments: new QueryArguments(
+                 new QueryArgument<NonNullGraphType<ProdutoInputType>> { Name = "produto" }),
+             resolve: context =>
+             {
+                 var produtoParam = context.GetArgument<Produto>("produto");
+                 var produto = produtoService.Obter(produtoParam.CodigoBarras);
+
+                 if (produto == null)
+                 {
+                     context.Errors.Add(new ExecutionError("Não foi possível encontrar esse produto!"));
+                     return null;
                  }
-       );
+
+                 produtoService.Atualizar(produtoParam);
+                 return $"Produto com o código:: {produto.CodigoBarras} atualizado com sucesso!";
+             });
+
+
+
+            Field<StringGraphType>(
+            "deleteProduto",
+            arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "codigoBarras" }),
+            resolve: context =>
+            {
+                var codigoBarras = context.GetArgument<string>("codigoBarras");
+                var produto = produtoService.Obter(codigoBarras);
+
+                if (produto == null)
+                {
+                    context.Errors.Add(new ExecutionError("Não foi possível encontrar esse produto!"));
+                    return null;
+                }
+
+                produtoService.Excluir(produto.CodigoBarras);
+                return $"Produto com o código:: {produto.CodigoBarras} deletado com sucesso!";
+            }
+);
         }
     }
 }
